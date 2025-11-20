faf37890ab7ef23fe16c32ae39507ea0b3c5a361
sinri <e.joshua.s.e@gmail.com>
2025-11-20 19:45:17 +0800

    Set up module-info.java for Redis integration module and update version to 5.0.0-rc.1 in pom.xml
    Signed-off-by: sinri <e.joshua.s.e@gmail.com>

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module io.github.sinri.keel.integration.redis {
+    requires io.github.sinri.keel.base;
+    requires io.github.sinri.keel.core;
+    requires io.vertx.core;
+    requires io.vertx.redis.client;
+    requires static org.jetbrains.annotations;
+
+    exports io.github.sinri.keel.integration.redis.kit;
+    exports io.github.sinri.keel.integration.redis.cache;
+}
