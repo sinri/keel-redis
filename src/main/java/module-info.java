e4285498aa81b33c2c72552f425dc4db223fa9c3
sinri <e.joshua.s.e@gmail.com>
2025-12-31 18:21:07 +0800

    build: Bump project version to 5.0.0-rc.18 and update dependencies
    - Updated `keel-core` to 5.0.0-rc.25.
    - Adjusted `module-info.java` to declare transitive dependencies for improved modularity.
    
    Signed-off-by: sinri <e.joshua.s.e@gmail.com>

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module io.github.sinri.keel.integration.redis {
-    requires io.github.sinri.keel.base;
-    requires io.github.sinri.keel.core;
-    requires io.vertx.core;
+    requires transitive io.github.sinri.keel.base;
+    requires transitive io.github.sinri.keel.core;
+    requires transitive io.vertx.core;
     requires transitive io.vertx.redis.client;
     requires static org.jetbrains.annotations;
 
